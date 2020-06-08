ab370f835bcb9ef5ef6ba8fedb82c6f366bacbe1
Moe <moepua0123@gmai.com>
2020-06-08 15:59:05 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 西山萌花 on 2020/06/08.
+//  Copyright © 2020 moeka.nishiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
